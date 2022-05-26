af5b7b35138c665e9507206057a7cf86e0fda49a
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2022-05-26 10:50:07 -0300

    final

---
+++ example/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:mifare_nfc_classic/mifare_nfc_classic.dart';
+
+import 'package:mifare_nfc_classic_example/example_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final isAvailable = MifareNfcClassic.availability;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder(
+        future: isAvailable,
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            switch (snapshot.data) {
+              case AVAILABILITY.AVAILABLE:
+                return ExamplePage();
+
+              case AVAILABILITY.NOT_ENABLED:
+                return Scaffold(
+                  body: Center(
+                    child: Text('NFC Not Enabled.'),
+                  ),
+                );
+              case AVAILABILITY.NOT_SUPPORTED:
+                return Scaffold(
+                  body: Center(
+                    child: Text('NFC Not Supported.'),
+                  ),
+                );
+
+              default:
+                return Scaffold(
+                  body: Center(
+                    child: Text('How?'),
+                  ),
+                );
+            }
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
+  }
+}
